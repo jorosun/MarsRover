e9cc94409752ca0fe3e63eca69212f9602517f74
Jennifer Orosun <orosunj@gmail.com>
2023-06-13 16:08:38 +0100

    refactored createPlateau test

--- src/plateau.test.ts
+++ src/plateau.test.ts
@@ -1,7 +1,6 @@
 import { createPlateau } from "../src/plateau";
 describe("test createPlateau function", () => {
-  it("should create an array size 5 x 5", () => {
-    const actual = createPlateau("55");
-    expect(actual).toEqual({ x: 5, y: 5 });
+  test("should create an array size 5 x 5", () => {
+    expect(createPlateau("55")).toEqual({ x: 5, y: 5 });
   });
 });
