a73d40e5a76a9d0ee54e1eee2105edcdac394dde
Jennifer Orosun <orosunj@gmail.com>
2023-06-14 21:26:34 +0100

    added file userInterface.ts

---
+++ src/userInterface.ts
@@ -0,0 +1,9 @@
+export function getGridSize(): string {
+  return "55";
+}
+export function getStartingPosition(): string {
+  return "12N";
+}
+export function getInstructions(): string {
+  return "LMLMLMLMM";
+}
